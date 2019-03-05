da365ae2c43e6cfd32fa34bcff7fb5194a08e10d
David Susco <dsusco@clarku.edu>
2019-03-05 14:52:53 -0500

    added lib file to append scss to path of project using the gem

---
+++ lib/baseline-scss.rb
@@ -0,0 +1,11 @@
+require 'sass'
+
+module BaselineScss
+  if defined?(Rails) && defined?(Rails::Engine)
+    class Engine < ::Rails::Engine
+      config.assets.paths << File.expand_path('../src', __FILE__)
+    end
+  else
+    Sass.load_paths << File.expand_path('../src/scss', __FILE__)
+  end
+end

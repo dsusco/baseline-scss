95a1291b89cc95df957da063d3ebb44108d22168
David Susco <dsusco@clarku.edu>
2023-02-16 13:00:43 -0500

    added support for jekyll

--- baseline-scss.gemspec
+++ baseline-scss.gemspec
@@ -14,5 +14,4 @@ Gem::Specification.new do |s|
   s.require_paths = ['lib']
 
   s.add_development_dependency 'rake'
-  s.add_development_dependency 'sass'
 end
--- lib/baseline_scss.rb
+++ lib/baseline_scss.rb
@@ -1,5 +1,23 @@
 module BaselineScss
-  if defined?(Rails) && defined?(Rails::Engine)
+  if defined?(Jekyll)
+    Jekyll::Command.class_eval do
+      class << self
+        def configuration_from_options(options)
+          return options if options.is_a?(Jekyll::Configuration)
+
+          options = Jekyll.configuration(options)
+
+          begin
+            options['sass']['load_paths'] << File.expand_path('../src', __dir__)
+          rescue
+            options.merge({ 'sass' => { 'load_paths' => [File.expand_path('../src', __dir__)] } })
+          end
+
+          options
+        end
+      end
+    end
+  elsif defined?(Rails) && defined?(Rails::Engine)
     class Engine < ::Rails::Engine
       initializer 'baseline-scss.paths', group: :all do |app|
         app.config.assets.paths << File.expand_path('../src', __dir__)
@@ -10,6 +28,13 @@ module BaselineScss
       require 'sass'
 
       Sass.load_paths << File.expand_path('../src', __dir__)
+    rescue LoadError
+    end
+
+    begin
+      require 'sassc'
+
+      SassC.load_paths << File.expand_path('../src', __dir__)
     rescue LoadError
     end
   end
